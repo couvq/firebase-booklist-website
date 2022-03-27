cbb6a65985e77e2838605bbe0f0701edee46a7d0
couvelaireq <quentin.couvelaire@hope.edu>
2022-03-27 12:34:51 -0400

    now maps and displays all books onload

--- src/components/FilterPage.js
+++ src/components/FilterPage.js
@@ -9,9 +9,11 @@ import { getAllBooks } from "../firebase/getAllBooks";
 const FilterPage = () => {
 
     const [year, setYear] = useState('All');
+    const [books, setBooks] = useState([]);
 
     useEffect(() => { // initially populate all of our books
         getAllBooks().then((books) => {
+            setBooks(books);
             console.log(books);
         });
     }, []);
@@ -54,7 +56,7 @@ const FilterPage = () => {
         </FormControl>
 
           <Grid container spacing={4} sx={{ paddingTop: "2rem"}}>
-            <Grid item xs={12} sm={6} md={6} lg={4}>
+            {/* <Grid item xs={12} sm={6} md={6} lg={4}>
               <BookCard
                 ImageURL={
                   "https://m.media-amazon.com/images/I/51mN3bY0JjL._SY346_.jpg"
@@ -68,39 +70,26 @@ const FilterPage = () => {
                   "https://www.amazon.com/Subtle-Art-Not-Giving-Counterintuitive-ebook/dp/B019MMUA8S"
                 }
               />
-            </Grid>
-
+            </Grid> */}
+            {books.map((book) => {
+          return (
             <Grid item xs={12} sm={6} md={6} lg={4}>
               <BookCard
                 ImageURL={
-                  "https://m.media-amazon.com/images/I/51mN3bY0JjL._SY346_.jpg"
+                  book.ImageURL
                 }
-                Title={"The Subtle Art of Not Giving a Fuck"}
-                Author={"Mark Manson"}
+                Title={book.Title}
+                Author={book.Author}
                 Recommendation={
-                  "I would recommend this to anyone really, it was a great read"
+                  book.Recommendation
                 }
                 PurchaseLink={
-                  "https://www.amazon.com/Subtle-Art-Not-Giving-Counterintuitive-ebook/dp/B019MMUA8S"
+                  book.PurchaseLink
                 }
               />
             </Grid>
-
-            <Grid item xs={12} sm={6} md={6} lg={4}>
-              <BookCard
-                ImageURL={
-                  "https://m.media-amazon.com/images/I/51mN3bY0JjL._SY346_.jpg"
-                }
-                Title={"The Subtle Art of Not Giving a Fuck"}
-                Author={"Mark Manson"}
-                Recommendation={
-                  "I would recommend this to anyone really, it was a great read"
-                }
-                PurchaseLink={
-                  "https://www.amazon.com/Subtle-Art-Not-Giving-Counterintuitive-ebook/dp/B019MMUA8S"
-                }
-              />
-            </Grid>
+          );
+        })}
           </Grid>
         </div>
       </div>
