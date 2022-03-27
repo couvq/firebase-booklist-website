a33dc30c3bc6a52072d5ccdb0f163f8c9efe58bf
couvelaireq <quentin.couvelaire@hope.edu>
2022-03-27 08:54:13 -0400

    added a components folder with a blank NavBar.js file for now

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import NavBar from './components/NavBar';
 
 const App = () => {
 
   return (
-    <div>Hello World!</div>
+    <>
+        <NavBar />
+    </>
   );
 }
 
