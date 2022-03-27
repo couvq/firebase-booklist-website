2660875fac4193ba4701ce26cd842e3fe2e3632b
couvelaireq <quentin.couvelaire@hope.edu>
2022-03-27 09:31:50 -0400

    FilterPage is now fullscreen but looks like shit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavBar from './components/NavBar';
+import FilterPage from './components/FilterPage';
 
 const App = () => {
 
   return (
     <>
         <NavBar />
+        <FilterPage />
     </>
   );
 }
---
+++ src/components/FilterPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const FilterPage = () => {
+  return (
+    <>
+      <div style={{
+          width: "100%",
+          height: "100vh",
+          background: 'red'
+      }}>
+        <h1>This is the FilterPage Component</h1>
+      </div>
+    </>
+  );
+};
+
+export default FilterPage;
